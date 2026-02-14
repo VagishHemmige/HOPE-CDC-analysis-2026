f4d7885aee5209693311918493cb5ab6df6647bb
Vagish Hemmige <vahemmig@montefiore.org>
2026-02-13 22:38:21 -0500

    Cleaning up join file

--- R/join_tracts_to_united_buffers.R
+++ R/join_tracts_to_united_buffers.R
@@ -6,36 +6,6 @@ for (organ_loop in organ_list){
     for (year_loop in year_list){
       
       message(paste("Currently on:", organ_loop, "", distance_loop, "for year:", year_loop))
-      
-      #ST join to individual tracts and the united transplant center geography to avoid duplication
-      tracts_in_buffers_active_united[[organ_loop]][[distance_loop]][[year_loop]] <- 
-        st_join(Merged_tracts[[year_loop]], 
-                Transplant_centers_active_buffer_united[[organ_loop]][[distance_loop]][[year_loop]], 
-                join = st_intersects)
-      
-      tracts_in_buffers_HIV_united[[organ_loop]][[distance_loop]][[year_loop]] <- 
-        st_join(Merged_tracts[[year_loop]], 
-                Transplant_centers_HIV_buffer_united[[organ_loop]][[distance_loop]][[year_loop]], 
-                join = st_intersects)
-      
-      tracts_in_buffers_HOPE_united[[organ_loop]][[distance_loop]][[year_loop]] <- 
-        st_join(Merged_tracts[[year_loop]], 
-                Transplant_centers_active_buffer_united[[organ_loop]][[distance_loop]][[year_loop]], 
-                join = st_intersects)
-    }}}
-
-
-#Join tracts to united buffers
-
-
-
-
-
-for (organ_loop in organ_list{
-  for (distance_loop in c("60min", "100mile", "200mile")){
-    for (year_loop in year_list){
-      
-      message(paste("Currently on:", organ_loop, "", distance_loop, "for year:", year_loop))
       message("Calculating tracts for active...")
       
       #ST join to individual tracts and the united transplant center geography to avoid duplication
@@ -53,6 +23,9 @@ for (organ_loop in organ_list{
       message("Calculating tracts for HOPE...")
       tracts_in_buffers_HOPE_united[[organ_loop]][[distance_loop]][[year_loop]] <- 
         st_join(Merged_tracts[[year_loop]], 
-                Transplant_centers_HOPE_buffer_united[[organ_loop]][[distance_loop]][[year_loop]], 
+                Transplant_centers_active_buffer_united[[organ_loop]][[distance_loop]][[year_loop]], 
                 join = st_intersects)
     }}}
+
+
+=

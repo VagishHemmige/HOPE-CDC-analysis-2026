b5c83965c1ead8f4c999c17b0a4d68318df21bdb
Vagish Hemmige <vahemmig@montefiore.org>
2026-02-14 13:44:47 -0500

    Functionalize tables

--- R/functions.R
+++ R/functions.R
@@ -9,37 +9,144 @@ set_60_min_isochrone_at_7AM<-function(SFObject, year) {
         st_transform(5070)
     )
   }else{
-  tempDF<-SFObject%>%
-    mutate(TimeZone=tz_lookup(., method="accurate"))%>%
-    st_transform(4326)
-  
-  FinalDF <- vector("list", length(timezones))
-  
-  
-  for(i in 1:length(timezones))
-  {
-    TZ<-converted_times[[year]][[i,1]]
+    tempDF<-SFObject%>%
+      mutate(TimeZone=tz_lookup(., method="accurate"))%>%
+      st_transform(4326)
     
-    UTCTime<-format(converted_times[[year]][[i,3]], "%Y-%m-%dT%H:%M:%SZ")
+    FinalDF <- vector("list", length(timezones))
     
-    if (nrow(filter(tempDF,TimeZone==TZ))>0)
+    
+    for(i in 1:length(timezones))
     {
-      temp<-tempDF%>%
-        filter(TimeZone==TZ)%>%
-        mb_isochrone(time = 60, 
-                     profile = "driving-traffic",
-                     depart_at = UTCTime,
-                     id_column = "OTCCode")%>%
-        rename(OTCName=id)
-      FinalDF[[i]]<-temp
+      TZ<-converted_times[[year]][[i,1]]
+      
+      UTCTime<-format(converted_times[[year]][[i,3]], "%Y-%m-%dT%H:%M:%SZ")
+      
+      if (nrow(filter(tempDF,TimeZone==TZ))>0)
+      {
+        temp<-tempDF%>%
+          filter(TimeZone==TZ)%>%
+          mb_isochrone(time = 60, 
+                       profile = "driving-traffic",
+                       depart_at = UTCTime,
+                       id_column = "OTCCode")%>%
+          rename(OTCName=id)
+        FinalDF[[i]]<-temp
+      }
     }
-  }
-  
-  FinalDF%>%
-    bind_rows()%>%
-    st_transform(5070)%>%
-    return()
+    
+    FinalDF%>%
+      bind_rows()%>%
+      st_transform(5070)%>%
+      return()
   }
 }
 
 #Function that converts results df into a gt format table
+
+convert_resultsdf_to_table<-function(resultsdf, 
+                                     year_list_fn=year_list,
+                                     row_values,
+                                     row_labels,
+                                     table_title){
+  
+  
+  resultsdf%>%
+    
+    #Reshape data set
+    pivot_wider(
+      names_from = Year,
+      values_from = c(Numerator, Denominator, Percentage),
+      names_vary = "slowest"
+    )%>%
+    
+    #Use the table defined above to turn Characteristic into a factor variable
+    mutate(Characteristic=factor(Characteristic,
+                                 levels = row_values,
+                                 labels = row_labels))%>%
+    arrange(Characteristic)%>%
+    
+    #Convert to a gt format table
+    gt()%>%
+    
+    #Groups columns by year with a spanner label
+    {
+      purrr::reduce(
+        year_list_fn,
+        ~ .x %>%
+          tab_spanner(
+            label = .y,
+            columns = matches(paste0("_", .y, "$"))
+          ),
+        .init = .
+      )
+    } %>%
+    
+    #Sets coluns to round to 0 decimal places for numbers and 1 decimal place for percentages
+    fmt_number(
+      columns = matches(paste0("Numerator_|Denominator_")),
+      decimals = 0,
+      use_seps = TRUE
+    ) %>%
+    fmt_number(
+      columns = matches("Percentage_"),
+      decimals = 1
+    )%>%tab_header(
+      title = table_title
+    )%>%
+    
+    #Removes "_year" from columns
+    cols_label_with(~ "Numerator",   columns = matches("^Numerator_")) %>%
+    cols_label_with(~ "Denominator", columns = matches("^Denominator_")) %>%
+    cols_label_with(~ "Percentage",  columns = matches("^Percentage_"))%>%
+    
+    #Shades columns by year for easier reading
+    
+    {
+      purrr::reduce(
+        seq_along(year_list_fn),
+        ~ .x %>%
+          tab_style(
+            style = cell_fill(
+              color = colorRampPalette(c("lightgray","lightblue"))(
+                length(year_list_fn)
+              )[.y]            ),
+            locations = cells_body(
+              columns = matches(paste0("_", year_list_fn[.y], "$"))
+            )
+          ),
+        .init = .
+      )
+    }%>%
+    
+    #Add separators between years
+    {
+      purrr::reduce(
+        seq_len(length(year_list_fn) - 1),
+        ~ .x %>%
+          tab_style(
+            style = cell_borders(
+              sides = "right",
+              weight = px(2),
+              color = "black"
+            ),
+            locations = cells_body(
+              columns = matches(paste0("Percentage_", year_list_fn[.y], "$"))
+            )
+          ) %>%
+          tab_style(
+            style = cell_borders(
+              sides = "left",
+              weight = px(2),
+              color = "black"
+            ),
+            locations = cells_body(
+              columns = matches(paste0("Numerator_", year_list_fn[.y + 1], "$"))
+            )
+          ),
+        .init = .
+      )
+    }
+  
+  
+}
--- R/tables.R
+++ R/tables.R
@@ -26,6 +26,7 @@ table_factor_values<-expand.grid(
     )
   )
 
+#Create tables as docx
 
 for (organ_loop in organ_list)
 {
@@ -33,178 +34,18 @@ for (organ_loop in organ_list)
   
   
   Results_HIV_df[[organ_loop]]%>%
-    
-    #Reshape data set
-    pivot_wider(
-      names_from = Year,
-      values_from = c(Numerator, Denominator, Percentage),
-      names_vary = "slowest"
-      )%>%
-    
-    #Use the table defined above to turn Characteristic into a factor variable
-    mutate(Characteristic=factor(Characteristic,
-                                 levels=table_factor_values$value,
-                                 labels = table_factor_values$label))%>%
-    arrange(Characteristic)%>%
-    
-    #Convert to a gt format table
-    gt()%>%
-    
-    #Groups columns by year with a spanner label
-    {
-      purrr::reduce(
-        year_list,
-        ~ .x %>%
-          tab_spanner(
-            label = .y,
-            columns = matches(paste0("_", .y, "$"))
-          ),
-        .init = .
-      )
-    } %>%
-    
-    #Sets coluns to round to 0 decimal places for numbers and 1 decimal place for percentages
-    fmt_number(
-      columns = matches(paste0("Numerator_|Denominator_")),
-      decimals = 0,
-      use_seps = TRUE
-    ) %>%
-    fmt_number(
-      columns = matches("Percentage_"),
-      decimals = 1
-    )%>%tab_header(
-      title = "Access of PLWH to transplant centers"
-    )%>%
-    
-    #Removes "_year" from columns
-    cols_label_with(~ "Numerator",   columns = matches("^Numerator_")) %>%
-    cols_label_with(~ "Denominator", columns = matches("^Denominator_")) %>%
-    cols_label_with(~ "Percentage",  columns = matches("^Percentage_"))%>%
-    
-    #Shades columns by year for easier reading
-    
-    {
-      purrr::reduce(
-        seq_along(year_list),
-        ~ .x %>%
-          tab_style(
-            style = cell_fill(
-              color = colorRampPalette(c("lightgray","lightblue"))(length(year_list))[.y]
-            ),
-            locations = cells_body(
-              columns = matches(paste0("_", year_list[.y], "$"))
-            )
-          ),
-        .init = .
-      )
-    }%>%
-      
-      #Add separators between years
-      {
-        purrr::reduce(
-          seq_len(length(year_list) - 1),
-          ~ .x %>%
-            tab_style(
-              style = cell_borders(
-                sides = "right",
-                weight = px(2),
-                color = "black"
-              ),
-              locations = cells_body(
-                columns = matches(paste0("Percentage_", year_list[.y], "$"))
-              )
-            ) %>%
-            tab_style(
-              style = cell_borders(
-                sides = "left",
-                weight = px(2),
-                color = "black"
-              ),
-              locations = cells_body(
-                columns = matches(paste0("Numerator_", year_list[.y + 1], "$"))
-              )
-            ),
-          .init = .
-        )
-      }
-  
-  %>%
+    convert_resultsdf_to_table(year_list_fn=year_list,
+                               row_values = table_factor_values$value,
+                               row_labels = table_factor_values$label,
+                               table_title= "Access to transplant in PLWH")%>%
     gtsave(paste0("tables/", organ_loop,"/",organ_loop, " analysis results Table 1.docx"))
   
   Results_nonHIV_df%>%
-    pivot_wider(
-      names_from = Year,
-      values_from = c(Numerator, Denominator, Percentage))%>%
-    select(Characteristic, 
-           Numerator_2017, Denominator_2017, Percentage_2017, 
-           Numerator_2022, Denominator_2022, Percentage_2022)%>%
-    mutate(Characteristic=factor(Characteristic,
-                                 levels=c("active 50mile","active 100mile", "active 200mile", "active 60min",
-                                          "HIV 50mile","HIV 100mile", "HIV 200mile", "HIV 60min",
-                                          "HOPE 50mile","HOPE 100mile", "HOPE 200mile", "HOPE 60min"),
-                                 labels = c("Active kidney center 50 mile radius",
-                                            "Active kidney center 100 mile radius", 
-                                            "Active kidney center 200 mile radius", 
-                                            "Active kidney center 60 min radius",
-                                            "Active HIV R+ kidney center 50 mile radius",
-                                            "Active HIV R+ kidney center 100 mile radius", 
-                                            "Active HIV R+ kidney center 200 mile radius", 
-                                            "Active HIV R+ kidney center 60 min radius",
-                                            "Active HOPE D+ kidney center 50 mile radius",
-                                            "Active HOPE D+ kidney center 100 mile radius",
-                                            "Active HOPE D+ kidney center 200 mile radius",
-                                            "Active HOPE D+ kidney center 60 min radius"
-                                            
-                                            
-                                 )))%>%
-    arrange(Characteristic)%>%
-    gt() %>%
-    tab_spanner(
-      label = "2017", 
-      columns = vars(Numerator_2017, Denominator_2017, Percentage_2017)
-    ) %>%
-    tab_spanner(
-      label = "2022", 
-      columns = vars(Numerator_2022, Denominator_2022, Percentage_2022)
-    )%>%
-    fmt_number(
-      columns = vars(Numerator_2017, Denominator_2017, 
-                     Numerator_2022, Denominator_2022),
-      decimals = 0,  
-      use_seps = TRUE
-    )%>%
-    tab_header(
-      title = "Access of people without HIV to transplant centers in 2017 and 2022"  # Title of your table
-    )%>%
-    cols_label(
-      Characteristic = "Characteristic",
-      Numerator_2017 = "Numerator",
-      Denominator_2017 = "Denominator",
-      Percentage_2017 = "Percentage",
-      Numerator_2022 = "Numerator",
-      Denominator_2022 = "Denominator",
-      Percentage_2022 = "Percentage"
-    )%>%
-    # Style for 2017 columns (data cells)
-    tab_style(
-      style = cell_fill(color = "lightgray"),
-      locations = cells_body(columns = vars(Numerator_2017, Denominator_2017, Percentage_2017))
-    ) %>%
-    # Style for 2022 columns (data cells)
-    tab_style(
-      style = cell_fill(color = "lightblue"),
-      locations = cells_body(columns = vars(Numerator_2022, Denominator_2022, Percentage_2022))
-    ) %>%
-    # Add borders to separate the two groups (2017 and 2022)
-    tab_style(
-      style = cell_borders(sides = c("right"), weight = px(2), color = "black"),
-      locations = cells_body(columns = vars(Numerator_2017, Denominator_2017, Percentage_2017))
-    ) %>%
-    tab_style(
-      style = cell_borders(sides = c("left"), weight = px(2), color = "black"),
-      locations = cells_body(columns = vars(Numerator_2022, Denominator_2022, Percentage_2022))
-    )%>%
-    gtsave("Kidney analysis results/Table 2.docx")
+    convert_resultsdf_to_table(year_list_fn=year_list,
+                               row_values = table_factor_values$value,
+                               row_labels = table_factor_values$label,
+                               table_title= "Access to transplant in people without HIV")%>%
+    gtsave(paste0("tables/", organ_loop,"/",organ_loop, " analysis results Table 2.docx"))
   
   
 }

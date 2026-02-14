1bd8dfd8e84a3d8f216ca8d7454abed9fa802e57
Vagish Hemmige <vahemmig@montefiore.org>
2026-02-14 16:05:47 -0500

    Add map-generating code to figures

--- R/figures.R
+++ R/figures.R
@@ -22,19 +22,34 @@ us_state_transformed<-us_states%>%
 
 
 Activemap<-list()
+HIV_map<-list()
+
 for (organ_loop in organ_list) {
   for (distance_loop in distance_list){
     for (year_loop in year_list) {
       
-      #Map all transplant centers and a 50-mile catchment area upon the dot map of HIV, 2017
+      #Map all transplant centers and a 50-mile catchment area upon the dot map of HIV
       Activemap[[organ_loop]][[year_loop]]<-ggplot() +
         geom_sf(data = county_dots_transformed[[year_loop]], size = 0.3, color = "navy", alpha = 0.5) +
         theme_minimal() +
-        labs(title = "Fifty-mile radius around kidney transplant centers, 2017", subtitle = "(1 dot = 100 HIV cases)")+
-        geom_sf(data=Transplant_centers_active_2017_50mile_buffer, color="red", fill=NA)+
+        labs(title = paste("Radius around kidney transplant centers,", year_loop),
+             subtitle = "(1 dot = 100 HIV cases)")+
+        geom_sf(data=Transplant_centers_active[[organ_loop]][[year_loop]], color="red", fill=NA)+
         geom_sf(data=us_state_transformed, fill=NA)
-      ggsave("Kidney analysis results/Active 50 mile map 2017.svg")
+      ggsave(paste0("figures/",organ_loop,"/Active ", organ_loop," ",distance_loop," map", year_loop,".svg"))
+     
       
+      
+      #Map transplant centers with HIV R+ and a 60 minute catchment area upon the dot map of HIV, 2013-2017
+      HIV_map[[organ_loop]][[year_loop]]<-ggplot() +
+        geom_sf(data = county_dots_transformed[[year_loop]], size = 0.3, color = "navy", alpha = 0.5) +
+        theme_minimal() +
+        labs(title = paste0(as.numeric(year_loop)-4, " - ", year_loop))+
+        geom_sf(data=Transplant_centers_HIV_buffer[[organ_loop]][[distance_loop]][[year_loop]], color="red", fill=NA)+
+        geom_sf(data=us_state_transformed, fill=NA)
+      ggsave(paste0("figures/",organ_loop,"/HIV ", organ_loop," ",distance_loop," map", year_loop,".svg"))
+      
+
     }
   }
 }
--- R/tables.R
+++ R/tables.R
@@ -2,50 +2,66 @@
 
 
 
-#Define table with labels based on values in distance_list and types of outcomes
 
-table_factor_values<-expand.grid(
-  Type = c("active", "HIV", "HOPE"),
-  Distance = distance_list,
-  stringsAsFactors = FALSE)%>%
-  mutate(value=paste(Type, Distance))%>%
-  mutate(
-    label = paste(
-      recode(Type,
-             active = paste("Active", organ_loop, "center"),
-             HIV    = paste("Active HIV R+", organ_loop, "center"),
-             HOPE   = paste("Active HOPE D+", organ_loop, "center")
-      ),
-      case_when(
-        stringr::str_detect(Distance, "mile$") ~ 
-          stringr::str_replace(Distance, "mile$", " mile radius"),
-        stringr::str_detect(Distance, "min$")  ~ 
-          stringr::str_replace(Distance, "min$", " min radius"),
-        TRUE ~ Distance
-      )
-    )
-  )
 
 #Create tables as docx
 
 for (organ_loop in organ_list)
 {
   
+  #Initialize file_path
+  file_path<-list()
+  file_path[["Table 1"]][[".docx"]]<-paste0("tables/", organ_loop,"/",organ_loop, " analysis results Table 1.docx")
+  file_path[["Table 2"]][[".docx"]]<-paste0("tables/", organ_loop,"/",organ_loop, " analysis results Table 2.docx")
+
   
+  #Define table with labels based on values in distance_list and types of outcomes
   
-  Results_HIV_df[[organ_loop]]%>%
+  table_factor_values<-expand.grid(
+    Type = c("active", "HIV", "HOPE"),
+    Distance = distance_list,
+    stringsAsFactors = FALSE)%>%
+    mutate(value=paste(Type, Distance))%>%
+    mutate(
+      label = paste(
+        recode(Type,
+               active = paste("Active", organ_loop, "center"),
+               HIV    = paste("Active HIV R+", organ_loop, "center"),
+               HOPE   = paste("Active HOPE D+", organ_loop, "center")
+        ),
+        case_when(
+          stringr::str_detect(Distance, "mile$") ~ 
+            stringr::str_replace(Distance, "mile$", " mile radius"),
+          stringr::str_detect(Distance, "min$")  ~ 
+            stringr::str_replace(Distance, "min$", " min radius"),
+          TRUE ~ Distance
+        )
+      )
+    )
+    
+  #Remove .docx files if they exist
+  if (file.exists(file_path[["Table 1"]][[".docx"]])) {
+    file.remove(file_path[["Table 1"]][[".docx"]])
+  }
+  
+  if (file.exists(file_path[["Table 2"]][[".docx"]])) {
+    file.remove(file_path[["Table 2"]][[".docx"]])
+  }
+  
+  #Create tables
+  Results_HIV_gt<-Results_HIV_df[[organ_loop]]%>%
     convert_resultsdf_to_table(year_list_fn=year_list,
                                row_values = table_factor_values$value,
                                row_labels = table_factor_values$label,
-                               table_title= "Access to transplant in PLWH")%>%
-    gtsave(paste0("tables/", organ_loop,"/",organ_loop, " analysis results Table 1.docx"))
+                               table_title= "Access to transplant in PLWH")
+    gtsave(Results_HIV_gt, file_path[["Table 1"]][[".docx"]])
   
-  Results_nonHIV_df%>%
+  Results_nonHIV_df[[organ_loop]]%>%
     convert_resultsdf_to_table(year_list_fn=year_list,
                                row_values = table_factor_values$value,
                                row_labels = table_factor_values$label,
-                               table_title= "Access to transplant in people without HIV")%>%
-    gtsave(paste0("tables/", organ_loop,"/",organ_loop, " analysis results Table 2.docx"))
+                               table_title= "Access to transplant in people without HIV")
+    gtsave(Results_HIV_gt, file_path[["Table 2"]][[".docx"]])
   
   
 }

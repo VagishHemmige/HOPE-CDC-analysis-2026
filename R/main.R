98b109ff6b322aef808feb90714a557f43d3ec1a
Vagish Hemmige <vahemmig@montefiore.org>
2026-02-15 15:55:46 -0500

    Create framework for website

--- R/main.R
+++ R/main.R
@@ -11,3 +11,6 @@ source("R/set_isochrones.R")
 source("R/unite_buffers.R")
 source("R/join_tracts_to_united_buffers.R")
 source("R/calculate_aggregated_results.R")
+
+source("R/tables.R")
+source("R/figures.R")
